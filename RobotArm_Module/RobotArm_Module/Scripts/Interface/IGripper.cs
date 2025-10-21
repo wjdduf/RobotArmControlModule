9afbc2d53e6900c826d172f63fd0e0acb9f4bbd5
WIN11-01\PC2507 <wjdduf9639@vi-tron.com>
2025-10-21 17:56:31 +0900

    Config Json 추가

---
+++ RobotArm_Module/RobotArm_Module/Scripts/Interface/IGripper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotArm_Module
+{
+    public interface IGripper
+    {
+        void Grip(Action onComplete = null);
+        void Release(Action onComplete = null);
+
+    }
+}
